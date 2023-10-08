0f542200a7f7578434ab743b09c0fa850b46ee2c
dev <dev@deb.deb>
2023-10-08 11:44:11 +0100

    Read message data

--- src/mqtt/mqtt.cpp
+++ src/mqtt/mqtt.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 #include "mqtt.hpp"
 
@@ -10,13 +11,16 @@ MQTT::~MQTT(){}
 
 void MQTT::on_connect(int rc){
     if(rc == MOSQ_ERR_SUCCESS){
-        subscribe(NULL, "/srf02/0");
         subscribe(NULL, "/srf02/1");
+        subscribe(NULL, "/srf02/2");
     }
 }
 
 void MQTT::on_message(const struct mosquitto_message* message){
-    std::cout << "Message received: " << (char*)message->payload << '\n';
+    char buf[2];
+    memcpy(buf, message->payload, 2*sizeof(char));
+    uint16_t measure = (uint8_t)buf[0] << 8 | (uint8_t)buf[1];
+    std::cout << "Topic " << message->topic << ": " << measure << '\n';
 }
 
 void MQTT::on_subscribe(int mid, int qos_count, const int* granted_qos){
